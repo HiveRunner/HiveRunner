ad6b7dc1ede9050152e92b70c25a16400d253ff2
per.ullberg <pelle.ullberg@klarna.com>
2015-06-17 15:22:11 +0200

    GBL-20935 Comment on hive/tez configuration Review task: https://stash.int.klarna.net/projects/ODIN/repos/hive-runner/pull-requests/2/overview?commentId=71837

--- src/main/java/com/klarna/hiverunner/StandaloneHiveServerContext.java
+++ src/main/java/com/klarna/hiverunner/StandaloneHiveServerContext.java
@@ -87,22 +87,42 @@ class StandaloneHiveServerContext implements HiveServerContext {
     }
 
     protected void configureExecutionEngine(HiveConf conf) {
-        // Tez configuration
+        /*
+        Enable tez execution engine
+         */
         conf.setVar(HiveConf.ConfVars.HIVE_EXECUTION_ENGINE, "tez");
 
+        /*
+        Tez local mode settings
+         */
         conf.setBoolean(TezConfiguration.TEZ_LOCAL_MODE, true);
         conf.set("fs.defaultFS", "file:///");
         conf.setBoolean(TezRuntimeConfiguration.TEZ_RUNTIME_OPTIMIZE_LOCAL_FETCH, true);
-        conf.set(TezConfiguration.TEZ_AM_WEBSERVICE_ENABLE, "false");
-        conf.set(TezConfiguration.DAG_RECOVERY_ENABLED, "false");
-        conf.set(TezConfiguration.TEZ_AM_NODE_BLACKLISTING_ENABLED, "false");
-        conf.set(TezConfiguration.TEZ_TASK_GET_TASK_SLEEP_INTERVAL_MS_MAX, "1");
+
+        /*
+            Tez will upload a hive-exec.jar to this location.
+            It looks like it will do this only once per test suite so it makes sense to keep this in a central location
+            rather than in the tmp dir of each test.
+         */
         conf.setVar(HiveConf.ConfVars.HIVE_JAR_DIRECTORY, "target/dependency");
         conf.setVar(HiveConf.ConfVars.HIVE_USER_INSTALL_DIR, "target/dependency");
+
+        /*
+        Set to be able to run tests offline
+         */
         conf.set(TezConfiguration.TEZ_AM_DISABLE_CLIENT_VERSION_CHECK, "true");
+
+        /*
+        General attempts to strip of unnecessary functionality to speed up test execution and increase stability
+         */
         conf.set(TezConfiguration.TEZ_AM_USE_CONCURRENT_DISPATCHER, "false");
         conf.set(TezConfiguration.TEZ_AM_CONTAINER_REUSE_ENABLED, "false");
         conf.set(TezConfiguration.DAG_RECOVERY_ENABLED, "false");
+        conf.set(TezConfiguration.TEZ_TASK_GET_TASK_SLEEP_INTERVAL_MS_MAX, "1");
+        conf.set(TezConfiguration.TEZ_AM_WEBSERVICE_ENABLE, "false");
+        conf.set(TezConfiguration.DAG_RECOVERY_ENABLED, "false");
+        conf.set(TezConfiguration.TEZ_AM_NODE_BLACKLISTING_ENABLED, "false");
+
     }
 
     protected void configureJavaSecurityRealm(HiveConf hiveConf) {
