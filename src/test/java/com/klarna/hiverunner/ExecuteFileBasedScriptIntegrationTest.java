6fb6cdbe827b0983aac36092f0cb07de82b6e1bb
Elliot West <ewest@hotels.com>
2016-09-15 16:31:06 +0100

    Remove tabs

--- src/test/java/com/klarna/hiverunner/ExecuteFileBasedScriptIntegrationTest.java
+++ src/test/java/com/klarna/hiverunner/ExecuteFileBasedScriptIntegrationTest.java
@@ -21,26 +21,26 @@ import com.klarna.hiverunner.annotations.HiveSQL;
 @RunWith(StandaloneHiveRunner.class)
 public class ExecuteFileBasedScriptIntegrationTest {
 
-	  @Rule
-	  public TemporaryFolder temp = new TemporaryFolder();
+      @Rule
+      public TemporaryFolder temp = new TemporaryFolder();
 
-	  @HiveSQL(files = {})
-	  private HiveShell hiveShell;
+      @HiveSQL(files = {})
+      private HiveShell hiveShell;
 
-	  @Test
-	  public void testExecuteFileBasedScript() throws IOException {
-	    File hqlScriptFile = temp.newFile("get_current_database.hql");
+      @Test
+      public void testExecuteFileBasedScript() throws IOException {
+        File hqlScriptFile = temp.newFile("get_current_database.hql");
 
-	    try (PrintStream out = new PrintStream(hqlScriptFile)) {
-	      out.println("select current_database(), NULL, 100;");
-	    }
+        try (PrintStream out = new PrintStream(hqlScriptFile)) {
+          out.println("select current_database(), NULL, 100;");
+        }
 
-	    hiveShell.execute(hqlScriptFile);
+        hiveShell.execute(hqlScriptFile);
 
-	    Charset optionalCharset = UTF_8;
-		List<String> results = hiveShell.executeQuery(optionalCharset, hqlScriptFile, " optional_column_delimiter ", "optional_null_replacement");
-	    
-		assertThat(results.size(), is(1));
-		assertThat(results.get(0), is("default optional_column_delimiter optional_null_replacement optional_column_delimiter 100"));
-	  }
+        Charset optionalCharset = UTF_8;
+        List<String> results = hiveShell.executeQuery(optionalCharset, hqlScriptFile, " optional_column_delimiter ", "optional_null_replacement");
+        
+        assertThat(results.size(), is(1));
+        assertThat(results.get(0), is("default optional_column_delimiter optional_null_replacement optional_column_delimiter 100"));
+      }
 }
