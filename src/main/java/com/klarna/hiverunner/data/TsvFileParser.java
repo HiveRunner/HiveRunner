c9442c25033e43fe2d3d8868bcb59794bb50628f
Vilhelm von Ehrenheim <vilhelm.ehrenheim@klarna.com>
2017-02-01 11:44:10 +0100

    Removed unused import.

--- src/main/java/com/klarna/hiverunner/data/TsvFileParser.java
+++ src/main/java/com/klarna/hiverunner/data/TsvFileParser.java
@@ -6,7 +6,6 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
