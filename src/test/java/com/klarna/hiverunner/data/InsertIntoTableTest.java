ddbf76d7b99219578dd54f72a43f1f687c52db0e
massdosage <massdosage@gmail.com>
2018-06-04 15:33:05 +0100

    - switch to non-deprecated versions of Mockito classes

--- src/test/java/com/klarna/hiverunner/data/InsertIntoTableTest.java
+++ src/test/java/com/klarna/hiverunner/data/InsertIntoTableTest.java
@@ -15,7 +15,7 @@
  */
 package com.klarna.hiverunner.data;
 
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 
 import java.io.File;
@@ -24,7 +24,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import com.google.common.collect.Multimap;
 
