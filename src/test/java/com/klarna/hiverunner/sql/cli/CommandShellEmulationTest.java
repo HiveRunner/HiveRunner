cd243c9d7d736dacbed28df01fd8ec17d628fa9c
Elliot West <ewest@hotels.com>
2018-06-07 21:14:37 +0100

    Moved tests

--- src/test/java/com/klarna/hiverunner/sql/cli/CommandShellEmulationTest.java
+++ src/test/java/com/klarna/hiverunner/sql/cli/CommandShellEmulationTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.klarna.hiverunner.sql;
+package com.klarna.hiverunner.sql.cli;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.isEmptyString;
