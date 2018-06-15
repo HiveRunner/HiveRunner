eba894cf9efa9f28b2387ed288e1505ad8250de2
Elliot West <ewest@hotels.com>
2018-06-15 17:04:16 +0100

    don't add extraneous newline

--- src/main/java/com/klarna/hiverunner/sql/cli/CommentUtil.java
+++ src/main/java/com/klarna/hiverunner/sql/cli/CommentUtil.java
@@ -18,18 +18,21 @@ package com.klarna.hiverunner.sql.cli;
 /** Utility methods for handling SQL comments. */
 public final class CommentUtil {
 
-	private CommentUtil() {
-	}
+  private CommentUtil() {}
 
-	public static String stripFullLineComments(String statement) {
-		StringBuilder newStatement = new StringBuilder(statement.length());
-		for (String line : statement.split("\n")) {
-			if (!line.trim().startsWith("--")) {
-				newStatement.append(line);
-				newStatement.append('\n');
-			}
-		}
-		return newStatement.toString().trim();
-	}
+  public static String stripFullLineComments(String statement) {
+    StringBuilder newStatement = new StringBuilder(statement.length());
+    String[] lines = statement.split("\n");
+    for (int l = 0; l < lines.length; l++) {
+      String line = lines[l];
+      if (!line.trim().startsWith("--")) {
+        newStatement.append(line);
+        if (l < lines.length - 2) {
+          newStatement.append('\n');
+        }
+      }
+    }
+    return newStatement.toString();
+  }
 
 }
