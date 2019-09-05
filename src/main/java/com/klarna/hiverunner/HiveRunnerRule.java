2476c0922a192144a07e092c3abb4d0734b51579
cmathiesen <cdm57@bath.ac.uk>
2019-09-05 14:48:01 +0100

    Test formatting

--- src/main/java/com/klarna/hiverunner/HiveRunnerRule.java
+++ src/main/java/com/klarna/hiverunner/HiveRunnerRule.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -31,60 +31,60 @@ import com.klarna.hiverunner.builder.Script;
  */
 public class HiveRunnerRule implements TestRule {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(HiveRunnerRule.class);
-    private final StandaloneHiveRunner runner;
+  private static final Logger LOGGER = LoggerFactory.getLogger(HiveRunnerRule.class);
+  private final StandaloneHiveRunner runner;
+  private final Object target;
+  private final Path testBaseDir;
+  private List<? extends Script> scriptsUnderTest;
+
+  HiveRunnerRule(StandaloneHiveRunner runner, Object target, Path testBaseDir) {
+    this.runner = runner;
+    this.target = target;
+    this.testBaseDir = testBaseDir;
+  }
+
+  public List<? extends Script> getScriptsUnderTest() {
+    return scriptsUnderTest;
+  }
+
+  public void setScriptsUnderTest(List<? extends Script> scriptsUnderTest) {
+    LOGGER.debug("Setting up hive runner scripts under test");
+    this.scriptsUnderTest = scriptsUnderTest;
+  }
+
+  @Override
+  public Statement apply(Statement base, Description description) {
+    LOGGER.debug("Running hive runner rule apply");
+    return new HiveRunnerRuleStatement(runner, target, base, testBaseDir);
+  }
+
+  class HiveRunnerRuleStatement extends Statement {
+
     private final Object target;
+    private final Statement base;
     private final Path testBaseDir;
-    private List<? extends Script> scriptsUnderTest;
+    private final StandaloneHiveRunner runner;
 
-    HiveRunnerRule(StandaloneHiveRunner runner, Object target, Path testBaseDir) {
-        this.runner = runner;
-        this.target = target;
-        this.testBaseDir = testBaseDir;
-    }
-
-    public List<? extends Script> getScriptsUnderTest() {
-        return scriptsUnderTest;
-    }
-
-    public void setScriptsUnderTest(List<? extends Script> scriptsUnderTest) {
-        LOGGER.debug("Setting up hive runner scripts under test");
-        this.scriptsUnderTest = scriptsUnderTest;
+    private HiveRunnerRuleStatement(
+        StandaloneHiveRunner runner,
+        Object target,
+        Statement base,
+        Path testBaseDir) {
+      this.runner = runner;
+      this.target = target;
+      this.base = base;
+      this.testBaseDir = testBaseDir;
     }
 
     @Override
-    public Statement apply(Statement base, Description description) {
-        LOGGER.debug("Running hive runner rule apply");
-        return new HiveRunnerRuleStatement(runner, target, base, testBaseDir);
+    public void evaluate() throws Throwable {
+      LOGGER.debug("Hive runner rule evaluate method");
+      HiveShellContainer container = runner.evaluateStatement(scriptsUnderTest, target, testBaseDir, base);
+      /**
+       * Script list will initially be null. 'evaluateStatement' sets up the script list.
+       * Need to set the value here to allow for mutation inside the mutantSwarmRule.
+       */
+      scriptsUnderTest = container.getScriptsUnderTest();
     }
-
-    class HiveRunnerRuleStatement extends Statement {
-
-        private final Object target;
-        private final Statement base;
-        private final Path testBaseDir;
-        private final StandaloneHiveRunner runner;
-
-        private HiveRunnerRuleStatement(
-            StandaloneHiveRunner runner,
-            Object target,
-            Statement base,
-            Path testBaseDir) {
-            this.runner = runner;
-            this.target = target;
-            this.base = base;
-            this.testBaseDir = testBaseDir;
-        }
-
-        @Override
-        public void evaluate() throws Throwable {
-            LOGGER.debug("Hive runner rule evaluate method");
-            HiveShellContainer container = runner.evaluateStatement(scriptsUnderTest, target, testBaseDir, base);
-            /**
-             * Script list will initially be null. 'evaluateStatement' sets up the script list.
-             * Need to set the value here to allow for mutation inside the mutantSwarmRule.
-             */
-            scriptsUnderTest = container.getScriptsUnderTest();
-        }
-    }
+  }
 }
