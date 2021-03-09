28e02f235107dc5891c4135946fe30f5a71389e8
shermosa <shermosa@hotels.com>
2021-03-09 16:27:16 +0000

    adding javadoc

--- src/test/java/com/klarna/hiverunner/AggregateViewTest.java
+++ src/test/java/com/klarna/hiverunner/AggregateViewTest.java
@@ -17,6 +17,11 @@ public class AggregateViewTest {
   @HiveSQL(files = {"AggregateViewTest/create_table.sql"}, autoStart = true)
   protected HiveShell shell;
 
+  /**
+   * Adding unit test to check that issue#70 (https://github.com/klarna/HiveRunner/issues/70) doesn't happen anymore.
+   * This bug is solved when upgrading HiveRunner to any version above 3.2.1 (at least 4.0.0).
+   */
+
   @Test
   public void aggregateView() {
 
