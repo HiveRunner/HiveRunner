d3a2bc947c708083e6e6114829a3bb68fe5cdb18
shermosa <shermosa@hotels.com>
2021-03-09 16:38:03 +0000

    tidy up

--- src/test/java/com/klarna/hiverunner/AggregateViewTest.java
+++ src/test/java/com/klarna/hiverunner/AggregateViewTest.java
@@ -24,12 +24,10 @@ public class AggregateViewTest {
 
   @Test
   public void aggregateView() {
-
     shell.insertInto("db", "mvtdescriptionchangeinfo").addRow("123", "testname", "REMOVED", "contents of test...", "hostname", "6/21/17","20").commit();
     List<String> result = shell.executeQuery("SELECT * FROM db.latesttestchangepairs");
     List<String> expected = Arrays.asList("testname\tREMOVED");
     assertThat(result,is(expected));
-
   }
 
 }
