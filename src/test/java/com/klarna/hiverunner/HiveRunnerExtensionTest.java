d5b3e2b0501f01e01f801102ee62514407e356bc
shermosa <shermosa@hotels.com>
2021-03-11 14:20:20 +0000

    asdfghj

--- src/test/java/com/klarna/hiverunner/HiveRunnerExtensionTest.java
+++ src/test/java/com/klarna/hiverunner/HiveRunnerExtensionTest.java
@@ -23,7 +23,6 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 
 import com.klarna.hiverunner.annotations.HiveSQL;
 
