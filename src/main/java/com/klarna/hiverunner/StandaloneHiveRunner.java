1b560199473fd0469a77b4d37170db6d51205616
shermosa <shermosa@hotels.com>
2021-03-11 17:05:02 +0000

    everything works and this time for real

--- src/main/java/com/klarna/hiverunner/StandaloneHiveRunner.java
+++ src/main/java/com/klarna/hiverunner/StandaloneHiveRunner.java
@@ -173,6 +173,9 @@ public class StandaloneHiveRunner extends BlockJUnit4ClassRunner {
      */
     public HiveShellContainer evaluateStatement(List<? extends Script> scripts, Object target,
         Path temporaryFolder, Statement base) throws Throwable {
+        if(scripts==null){
+            scripts = new ArrayList<>();
+        }
         container = null;
         File temporaryFile = temporaryFolder.toFile();
         if (!temporaryFile.exists()) {
