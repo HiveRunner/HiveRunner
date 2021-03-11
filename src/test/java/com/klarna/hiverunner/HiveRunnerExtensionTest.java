f332f51afe2089b0b409b992311e10eb0db0b927
shermosa <shermosa@hotels.com>
2021-03-11 15:25:24 +0000

    works

--- src/test/java/com/klarna/hiverunner/HiveRunnerExtensionTest.java
+++ src/test/java/com/klarna/hiverunner/HiveRunnerExtensionTest.java
@@ -29,7 +29,7 @@ import com.klarna.hiverunner.annotations.HiveSQL;
 @ExtendWith(HiveRunnerExtension.class)
 public class HiveRunnerExtensionTest {
 
-  @HiveSQL(files = { "hiveRunnerExtensionTest/test_query.sql" })
+  @HiveSQL(files = { "HiveRunnerExtensionTest/test_query.sql" })
   private HiveShell shell;
 
   @Test
